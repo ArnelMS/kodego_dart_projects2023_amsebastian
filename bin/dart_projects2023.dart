400bf64f356d8054e488a881eb71b26422cedd80
Arnel Sebastian <arnelmsebastian@gmail.com>
2023-01-04 23:20:00 +0800

    dart_first_commit

---
+++ bin/dart_projects2023.dart
@@ -0,0 +1,5 @@
+import 'package:dart_projects2023/dart_projects2023.dart' as dart_projects2023;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_projects2023.calculate()}!');
+}
---
+++ test/dart_projects2023_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_projects2023/dart_projects2023.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
